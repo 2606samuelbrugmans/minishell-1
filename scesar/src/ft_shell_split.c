8df653e86bc31ed366ed7be90953703b0f02c314
Stephen Cesar <scesar@fu-r10-p6.s19.be>
2025-04-26 00:16:12 +0200

    still have to test it

--- scesar/src/ft_shell_split.c
+++ scesar/src/ft_shell_split.c
@@ -1,6 +1,5 @@
 #include "../inc/minishell.h"
 
-// bool reach_next_arg(char *input, size_t index, int quote)
 
 size_t next_arg_len(char *input, size_t index, int quote)
 {
@@ -9,23 +8,31 @@ size_t next_arg_len(char *input, size_t index, int quote)
     len = 0;
     if(quote == SIMPLE_QUOTE)
     {
-        while(input[index] != "\'" || input[index] != '\0')
+        len++;
+        index++;
+        while(input[index] != '\'' && input[index] != '\0')
         {
             len++;
             index++;
         }
+        if (input[index] == '\'')
+            len++;
     }
     else if(quote == DOUBLE_QUOTE)
     {
-        while(input[index] != "\"" || input[index] != '\0')
+        len++;
+        index++;
+        while(input[index] != '\"' && input[index] != '\0')
         {
             len++;
             index++;
         }
+        if (input[index] == '\"')
+            len++;
     }
     else
     {
-        while(input[index] != " " || input[index] != '\0')
+        while(input[index] != ' ' && input[index] != '\0')
         {
             len++;
             index++;
@@ -34,23 +41,25 @@ size_t next_arg_len(char *input, size_t index, int quote)
     return(len);
 }
 
-void    put_elem_in_tab(char **tab, size_t tab_index, char *input, size_t input_index, int quote)
+void    put_elem_in_tab(char **tab, size_t tab_index, char *input, size_t *input_index, int quote)
 {
     char *elem;
     size_t elem_size;
     size_t elem_index;
 
     elem_index = 0;
-    elem_size = next_arg_len(input, input_index, quote) + 1;
+    elem_size = next_arg_len(input, *input_index, quote) + 1;
     elem = (char *)malloc(sizeof(char) * elem_size);
-    while(elem_index < elem_size + 1)
+    while(elem_index < elem_size - 1)
     {
-        elem[elem_index] = input[input_index];
+        elem[elem_index] = input[*input_index];
         elem_index++;
-        input_index++;
+        (*input_index)++;
     }
-    elem_index = '\0';
-    *tab[tab_index] = elem;
+    elem[elem_index] = '\0';
+    tab[tab_index] = elem;
+    if (quote == SIMPLE_QUOTE || quote == DOUBLE_QUOTE)
+        (*input_index)++;
 }
 
 size_t nbr_of_elem(char *input)
@@ -66,14 +75,14 @@ size_t nbr_of_elem(char *input)
             index++;
         while(input[index] != ' ' && input[index] != '\0')
         {
-            if (input[index] == "\'")
+            if (input[index] == '\'')
             {
-                while(input[index] != "\'" || input[index] != '\0')
+                while(input[index] != '\'' && input[index] != '\0')
                     index++;
             }
-            else if (input[index] == "\"")
+            else if (input[index] == '\"')
             {
-                while(input[index] != "\"" || input[index] != '\0')
+                while(input[index] != '\"' && input[index] != '\0')
                     index++;
             }
             index++;
@@ -94,18 +103,21 @@ char **ft_split_shell(char *input)
     input_index = 0;
     tab_size = nbr_of_elem(input) + 1;
     tab_of_elem = (char**)malloc(sizeof(char *) * (tab_size));
-    while (tab_size > 0)
+    while (tab_index < tab_size - 1)
     {
         while(input[input_index] == ' ')
-        input_index++;
+            input_index++;
         while(input[input_index] != ' ' && input[input_index] != '\0')
         {
-            if (input[input_index] == "\'")
-                put_elem_in_tab(tab_of_elem, tab_index, input, input_index, SIMPLE_QUOTE);
-            else if (input[input_index] == "\"")
-                put_elem_in_tab(tab_of_elem, tab_index, input, input_index, DOUBLE_QUOTE);
+            if (input[input_index] == '\'')
+                put_elem_in_tab(tab_of_elem, tab_index, input, &input_index, SIMPLE_QUOTE);
+            else if (input[input_index] == '\"')
+                put_elem_in_tab(tab_of_elem, tab_index, input, &input_index, DOUBLE_QUOTE);
             else
-                put_elem_in_tab(tab_of_elem, tab_index, input, input_index, 0);
+                put_elem_in_tab(tab_of_elem, tab_index, input, &input_index, 0);
         }
+        tab_index++;
     }
+    tab_of_elem[tab_index] = NULL;
+    return tab_of_elem;
 }
